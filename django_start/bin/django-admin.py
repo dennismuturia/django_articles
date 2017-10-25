841f94a0895e80eaf0f08050dee7b74b1336e8f4
dennismuturia <dmuturia2922@gmail.com>
2017-10-25 14:27:32 +0300

    This is theinitial commit

---
+++ django_start/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/dennis/PycharmProjects/django_start/bin/python3.6
+# EASY-INSTALL-SCRIPT: 'Django==1.11.6','django-admin.py'
+__requires__ = 'Django==1.11.6'
+__import__('pkg_resources').run_script('Django==1.11.6', 'django-admin.py')
