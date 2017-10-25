46f7f9aed56dd917133d94f8e7a48c15308b836e
dennismuturia <dmuturia2922@gmail.com>
2017-10-25 15:21:17 +0300

    Just done by making my migrations in the articles app. But first you have to confirm thatthat app exists in the settings.py

--- django_start/django_test/articles/models.py
+++ django_start/django_test/articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. And am currently doing that.
+class Article(models.Model):
+    title: models.CharField(max_length=200)
+    body: models.TextField()
+    likes: models.IntegerField()
+    pub_date: models.DateTimeField("Published Date")
