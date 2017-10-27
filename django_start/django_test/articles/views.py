9d3835c6b614ce9d71e70dcc96f31ae98810cf15
dennismuturia <dmuturia2922@gmail.com>
2017-10-27 11:10:00 +0300

    TRying to inject data from the views.py

--- django_start/django_test/articles/views.py
+++ django_start/django_test/articles/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here. Creating now!!
 def hello(request):
-    return render(request, ' hello.html')
+    my_variable = "Dennis"
+    years = 23
+    cities_array = ['Nairobi', 'Meru', 'Nyahururu']
+    return render(request, 'hello.html', {'my_var':my_variable, 'years':years, 'cities':cities_array})
